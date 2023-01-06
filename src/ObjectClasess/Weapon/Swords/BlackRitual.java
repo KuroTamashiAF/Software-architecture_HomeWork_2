97bc97135a4cf6b34f75193c6e6e8b93cf81e265
Fedorov <chugun777akk@mail.ru>
2023-01-06 16:11:11 +0700

    add class blackRitual

---
+++ src/ObjectClasess/Weapon/Swords/BlackRitual.java
@@ -0,0 +1,13 @@
+package ObjectClasess.Weapon.Swords;
+
+public class BlackRitual extends  Sword{
+    private  int damage = 20;
+    public BlackRitual(String name) {
+        super(name);
+    }
+
+    @Override
+    public int getDamage() {
+        return damage;
+    }
+}
