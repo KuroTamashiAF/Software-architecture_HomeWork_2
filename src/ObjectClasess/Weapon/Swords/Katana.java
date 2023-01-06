ce94c8c3ab627267e64abf46a07faa8d1528943f
Fedorov <chugun777akk@mail.ru>
2023-01-06 16:09:43 +0700

    add class katana

---
+++ src/ObjectClasess/Weapon/Swords/Katana.java
@@ -0,0 +1,12 @@
+package ObjectClasess.Weapon.Swords;
+
+public class Katana extends Sword{
+    private int damade  =30;
+    public Katana(String name) {
+        super(name);
+    }
+
+    public int getDamade() {
+        return damade;
+    }
+}
