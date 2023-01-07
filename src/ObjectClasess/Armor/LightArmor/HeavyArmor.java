5434b30b6e667a83be10f83a7be2d5c87f3d05e4
Fedorov <chugun777akk@mail.ru>
2023-01-07 07:50:38 +0700

    Add class heavy Armor

--- src/ObjectClasess/Armor/LightArmor/HeavyArmor.java
+++ src/ObjectClasess/Armor/LightArmor/HeavyArmor.java
@@ -1,4 +1,14 @@
 package ObjectClasess.Armor.LightArmor;
 
-public class HeavyArmor {
+import ObjectClasess.Armor.Armor;
+
+public class HeavyArmor extends Armor {
+    private double resistance  = 0.55;
+    public HeavyArmor(String name) {
+        super(name);
+    }
+
+    public double getResistance() {
+        return resistance;
+    }
 }
