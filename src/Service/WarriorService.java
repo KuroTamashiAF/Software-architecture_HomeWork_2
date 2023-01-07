4235ef52f18ea75f8139e41a710abaf17d4a6329
Fedorov <chugun777akk@mail.ru>
2023-01-07 08:07:42 +0700

    Add interface warrior service

---
+++ src/Service/WarriorService.java
@@ -0,0 +1,9 @@
+package Service;
+
+public interface WarriorService
+{
+    
+
+
+
+}
---
+++ src/Service/WarriorServiveImpl.java
@@ -0,0 +1,11 @@
+package Service;
+
+import ObjectClasess.Warrior.Warrior;
+
+public class WarriorServiveImpl implements  WarriorService{
+    private Warrior war;
+
+    public WarriorServiveImpl(Warrior war) {
+        this.war = war;
+    }
+}
